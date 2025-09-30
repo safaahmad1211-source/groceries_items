f0df690a207b0a6413d6c3dfb6f95abadb2e3d75
Ahmad Safa <safaahmad1211@gmail.com>
2025-09-30 14:44:09 +0300

    see if i spent alot or a little

--- assignment-2.py
+++ assignment-2.py
@@ -8,5 +8,16 @@ while True:
         shopping_cart.append(item)
     else:
         print("Sorry, we dont have that item.")
+
+print(f"You bought:{shopping_cart}")
+total = 0
+for item in shopping_cart: 
+    total=total+groceries[item]
+print(f"total= ${total}")
+if total>10:
+    print("You spent alot!")
+
+else:
+    print("You spent a little")
+
        
-      
